57e829afc1bc475cc89d94a8837cff89cdfb68da
James T Peterson <jpeterson903@yahoo.com>
2015-11-17 13:52:20 -0800

    Add function to link list to markers

--- js/app-map.js
+++ js/app-map.js
@@ -60,16 +60,16 @@ var map;
 
 
 var markers = [];
+var myListener;
 var infowindow = new google.maps.InfoWindow();
 function Pin(map, name, lat, lng, id) {
-  //var contentString = '<b>' + name + '</b>';
   var marker;
-
 
   this.name = ko.observable(name);
   this.lat  = ko.observable(lat);
   this.lng  = ko.observable(lng);
   this.id   = ko.observable(id);
+  this.marker = ko.observable(marker);
 
 
   marker = new google.maps.Marker({
@@ -77,22 +77,11 @@ function Pin(map, name, lat, lng, id) {
     animation: google.maps.Animation.DROP,
     title: name,
     //icon: 'images/golfer.png',
-    map: map
-    //content: contentString
+    map: map,
+    id: id
   });
 
   marker.addListener('click', toggleBounce);
-  google.maps.event.addListener(map, 'click', function() {
-    infowindow.close();
-  });
-
-  // google.maps.event.addListener(marker, 'click', function() {
-  //   //infowindow.setContent(this.content);
-  //   //infowindow.open(map, this);
-  //   createInfoWindow(pins);
-  // })
-  	 //infowindow.setContent();
-     //infowindow.open(map, this);
 
 /**
  * Generates a random number and returns it as a string for OAuthentication
@@ -103,11 +92,11 @@ function nonce_generate() {
   return (Math.floor(Math.random() * 1e12).toString());
 }
 
-YELP_KEY = "0dt_RWZ_onDeRN_tx-JyhA";
-YELP_KEY_SECRET = "fo8iIXS_u_TjOil8uMnNTm8LdEc";
-YELP_TOKEN = "jRyu-QVU-IozwSQnZHW4u2U2Z0DVoGjd";
-YELP_TOKEN_SECRET = "AyCSVZzGk-1kxF1I34unTYKw0vc";
-var yelp_url = 'http://api.yelp.com/v2/search';
+  YELP_KEY = "0dt_RWZ_onDeRN_tx-JyhA";
+  YELP_KEY_SECRET = "fo8iIXS_u_TjOil8uMnNTm8LdEc";
+  YELP_TOKEN = "jRyu-QVU-IozwSQnZHW4u2U2Z0DVoGjd";
+  YELP_TOKEN_SECRET = "AyCSVZzGk-1kxF1I34unTYKw0vc";
+  var yelp_url = 'http://api.yelp.com/v2/search';
 
   var parameters = {
       oauth_consumer_key: YELP_KEY,
@@ -137,18 +126,12 @@ var yelp_url = 'http://api.yelp.com/v2/search';
         var link = '<a href=';
         var linku = '>';
         var contentString = '<a href=' + data.businesses[0].url + '>' + data.businesses[0].name + '</a>' + imago + data.businesses[0].image_url + sauce + '<br>' + imago + data.businesses[0].rating_img_url + sauce + data.businesses[0].review_count + ' reviews <br>';
-        //google.maps.event.addListener(marker, 'click', function() {
-        //infowindow.setContent(contentString);
 
       google.maps.event.addListener(marker, 'click', function() {
       infowindow.setContent(contentString);
-      infowindow.open(map, this);
+      infowindow.open(map, marker);
     });
 
-        //infowindow.open(map, this);
-        //var json = data;
-        //console.log(json);
-        //callBack(data);
       },
       error: function() {
         // Do stuff on fail
@@ -158,26 +141,6 @@ var yelp_url = 'http://api.yelp.com/v2/search';
 
     // Send AJAX query via jQuery library.
     $.ajax(settings);
-
-          //console.log(yelpContent);
-          //var contentString = '<a href=' + data.businesses[i].url + '>' + data.businesses[i].name + '</a>' + imago + data.businesses[i].image_url + sauce + '<br>' + imago + data.businesses[i].rating_img_url + sauce + data.businesses[i].review_count + ' reviews <br>';
-        // var imago = '<img src=';
-        // var sauce = '>';
-        // var link = '<a href=';
-        // var linku = '>';
-
-        //var contentString = '<a href=' + yelpContent.url + '>' + yelpContent.name + '</a>' + imago + yelpContent.image_url + sauce + '<br>' + imago + yelpContent.rating_img_url + sauce + yelpContent.review_count + ' reviews <br>';
-        //createInfoWindow(marker, contentString)
-        //var contentString = '<a href=' + dataContent.businesses[i].url + '>' + dataContent[i].name + '</a>' + imago + dataContent[i].image_url + sauce + '<br>' + imago + dataContent.businesses[i].rating_img_url + sauce + dataContent.businesses[i].review_count + ' reviews <br>';
-        //infowindow.setContent(contentString);
-        //infowindow.setContent('<a href=' + data.businesses[i].url + '>' + data.businesses[i].name + '</a>' + imago + data.businesses[i].image_url + sauce + '<br>' + imago + data.businesses[i].rating_img_url + sauce + data.businesses[i].review_count + ' reviews <br>');
-        //infowindow.open(map, marker);
-        //console.log(data);
-        //console.log(results.businesses[0].snippet_text);
-        //createInfoWindow(marker, contentString);
-      // google.maps.event.addListener(marker, 'click', function() {
-      // infowindow.setContent(contentString);
-      // infowindow.open(map, this);
 
   this.isVisible = ko.observable(false);
 
@@ -191,28 +154,35 @@ var yelp_url = 'http://api.yelp.com/v2/search';
 
   this.isVisible(true);
 
-  markers.push(marker);
+    markers.push(marker);
+    console.log(markers);
 }
 
     // listClick = function() {
     // google.maps.event.trigger(marker, 'click');
     // }
-listClick = function() {
-      for (var i = 0; i < markers.length; i++) {
-        google.maps.event.trigger(markers[i], 'click');
-      }
-    }
+// listClick = function() {
+//       for (var i = 0; i < markers.length; i++) {
+//         google.maps.event.trigger(markers[i], 'click');
+//       }
+//     }
+
+var i = 0;
 
 
-
-// function createInfoWindow(marker, contentString) {
-//   google.maps.event.addListener(marker, 'click', function() {
-//     return function(){
-//     infowindow.setContent(contentString);
-//     infowindow.open(map, this);
-//   }
-// }(contentString));
+// function listClick() {
+//   if (self.pin.name === markers.name)
+//     google.maps.event.trigger(markers[0].name, "click")
 // }
+function listClick(id) {
+      google.maps.event.trigger(markers[id], 'click');
+}
+
+
+
+google.maps.event.addListener(map, 'click', function() {
+  infowindow.close();
+});
 
 function toggleBounce() {
 	var self = this;
@@ -226,14 +196,14 @@ function toggleBounce() {
 	}
 }
 
-var pins = [
+// var pins = [
 
-        new Pin(map, 'Journey At Pechanga', 33.453159, -117.106527, 1),
-        new Pin(map, 'Temecula Creek Inn', 33.4676068, -117.1291816, 2),
-        new Pin(map, 'The Golf Club at Rancho California', 33.562563, -117.145126, 3),
-        new Pin(map, 'Redhawk Golf Club', 33.468955, -117.091304, 4),
-        new Pin(map, 'The Legends Golf Club', 33.515210, -117.113069, 5)
-];
+//         new Pin(map, 'Journey At Pechanga', 33.453159, -117.106527, 1),
+//         new Pin(map, 'Temecula Creek Inn', 33.4676068, -117.1291816, 2),
+//         new Pin(map, 'The Golf Club at Rancho California', 33.562563, -117.145126, 3),
+//         new Pin(map, 'Redhawk Golf Club', 33.468955, -117.091304, 4),
+//         new Pin(map, 'The Legends Golf Club', 33.515210, -117.113069, 5)
+// ];
 
 var pinZ = [
 {
@@ -268,25 +238,26 @@ var ViewModel = function() {
 
   //infowindow.setContent(createContent(marker.name));
 
-	// self.pins = ko.observableArray([
-	//         new Pin(map, 'Temeku Hills Golf Club', 33.516145, -117.115446, '1'),
-	//         new Pin(map, 'Temecula Creek Inn', 33.4676068, -117.1291816, '2'),
-	//         new Pin(map, 'CrossCreek Golf Club', 33.496457, -117.225794, '3'),
-	//         new Pin(map, 'The Golf Club at Rancho California', 33.562563, -117.145126, '4'),
-	//         new Pin(map, 'Redhawk Golf Club', 33.468955, -117.091304, '5')
-	// ]);
+	self.pins = ko.observableArray([
+	        new Pin(map, 'Temeku Hills Golf Club', 33.516145, -117.115446, 0),
+	        new Pin(map, 'Temecula Creek Inn', 33.4676068, -117.1291816, 1),
+	        new Pin(map, 'CrossCreek Golf Club', 33.496457, -117.225794, 2),
+	        new Pin(map, 'The Golf Club at Rancho California', 33.562563, -117.145126, 3),
+	        new Pin(map, 'Redhawk Golf Club', 33.468955, -117.091304, 4)
+	]);
 
 	self.query = ko.observable('');
 
 	self.filterPins = ko.computed(function () {
 	    var search  = self.query().toLowerCase();
 
-	    return ko.utils.arrayFilter(pins, function (pin) {
+	    return ko.utils.arrayFilter(self.pins(), function (pin) {
 	        var doesMatch = pin.name().toLowerCase().indexOf(search) >= 0;
 
 	        pin.isVisible(doesMatch);
+          google.maps.event.trigger(pin.marker, 'click');
 
-	        return doesMatch;
+	       return doesMatch;
 	    });
 	});
 };
